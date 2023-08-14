2c982a79266c5cbce58c7a55aee38eae0093b6ac
lemreyes <67998813+lemreyes@users.noreply.github.com>
2023-08-15 06:50:56 +0800

    Navbar flex layout

--- src/app/Components/Navbar.tsx
+++ src/app/Components/Navbar.tsx
@@ -8,14 +8,14 @@ import cart_icon from "../../../public/cart.svg";
 
 export default function Navbar() {
   return (
-    <nav className="flex-col">
-      <Image src={logo} alt="logo" />
+    <nav className="min-h-screen max-w-fit flex flex-col justify-between">
+      <Image src={logo} alt="logo" className="mx-4 mt-8" />
       <div>
-        <Image src={list_icon} alt="list_icon" />
-        <Image src={replay_icon} alt="replay_icon" />
-        <Image src={stats_icon} alt="stats_icon" />
+        <Image src={list_icon} alt="list_icon" className="mx-4 my-2" />
+        <Image src={replay_icon} alt="replay_icon" className="mx-4 my-2" />
+        <Image src={stats_icon} alt="stats_icon" className="mx-4 my-2" />
       </div>
-      <Image src={cart_icon} alt="cart" />
+      <Image src={cart_icon} alt="cart" className="mx-4 mb-8" />
     </nav>
   );
 }
