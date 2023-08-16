ad2dd53a143b2d67f2023ab4031935870c277b1d
lemreyes <67998813+lemreyes@users.noreply.github.com>
2023-08-16 20:36:33 +0800

    heading name font color and bold

--- src/app/Components/Masterlist.tsx
+++ src/app/Components/Masterlist.tsx
@@ -4,7 +4,7 @@ export default function Masterlist() {
   return (
     <main className="ml-16 mt-8">
       <h1 className="text-4xl ">
-        <span>SyncShop</span> seamlessly share and shop together with real-time list updates
+        <span className="text-orange-600 font-bold">SyncShop</span> seamlessly share and shop together with real-time list updates
       </h1>
       <h2 className="text-lg">Fruit and vegetables</h2>
       <button>Avocado</button>
