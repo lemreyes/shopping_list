728c5a016d13f9fa5067f46af1db6dad853012f6
lemreyes <67998813+lemreyes@users.noreply.github.com>
2023-08-19 12:20:03 +0800

    Active list panel justification

--- src/app/Components/ActiveListPanel.tsx
+++ src/app/Components/ActiveListPanel.tsx
@@ -3,18 +3,20 @@ import ListSaveButton from "./ListSaveButton";
 
 export default function ActiveListPanel() {
   return (
-    <aside className="p-8 bg-gray-200">
-      <h2 className="font-bold text-2xl">Shopping List</h2>
-      <h3 className="text-sm mt-4">Fruits and vegetables</h3>
-      <ul className="text-lg">
-        <li>Avocado</li>
-        <li>Bananas</li>
-      </ul>
-      <h3 className="text-sm mt-4">Beverages</h3>
-      <ul className="text-lg">
-        <li>Milk</li>
-        <li>Orange Juice</li>
-      </ul>
+    <aside className="p-8 bg-gray-200 flex flex-col justify-between">
+      <div>
+        <h2 className="font-bold text-2xl">Shopping List</h2>
+        <h3 className="text-sm mt-4">Fruits and vegetables</h3>
+        <ul className="text-lg">
+          <li>Avocado</li>
+          <li>Bananas</li>
+        </ul>
+        <h3 className="text-sm mt-4">Beverages</h3>
+        <ul className="text-lg">
+          <li>Milk</li>
+          <li>Orange Juice</li>
+        </ul>
+      </div>
       <ListSaveButton />
     </aside>
   );
