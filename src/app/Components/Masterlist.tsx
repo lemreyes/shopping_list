b103db150ae83188de5c78158196bc9beecc85dc
lemreyes <67998813+lemreyes@users.noreply.github.com>
2023-08-19 12:44:36 +0800

    Search filter input placement

--- src/app/Components/Masterlist.tsx
+++ src/app/Components/Masterlist.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import Item from "./Item";
 
+import Image from "next/image";
+
+import search_icon from "../../../public/assets/search_icon.svg";
+
 export default function Masterlist() {
   return (
     <main className="ml-16 mt-8">
@@ -8,10 +12,20 @@ export default function Masterlist() {
         <span className="text-orange-600 font-bold">SyncShop</span> seamlessly
         share and shop together with real-time list updates
       </h1>
-      <h2 className="text-lg font-medium mt-8">Fruit and vegetables</h2>
+      <h2 className="font-bold text-2xl mt-8">Master List</h2>
+      <div className="flex flex-row">
+        <Image src={search_icon} alt="search" className="w-6 h-6 absolute translate-x-1 translate-y-3" />
+        <input
+          type="text"
+          placeholder="Search for an item"
+          className="mt-2 border rounded-lg border-gray-400 p-1 pl-8"
+        />
+      </div>
+
+      <h3 className="text-lg font-medium mt-8">Fruit and vegetables</h3>
       <Item label="Avocado" />
       <Item label="Bananas" />
-      <h2 className="text-lg font-medium mt-8">Beverages</h2>
+      <h3 className="text-lg font-medium mt-8">Beverages</h3>
       <Item label="Milk" />
       <Item label="Orange Juice" />
     </main>
