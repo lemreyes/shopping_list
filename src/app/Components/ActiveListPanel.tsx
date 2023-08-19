b8d9b76e5aeeda3ac0668e999dfb6d19c199e9a8
lemreyes <67998813+lemreyes@users.noreply.github.com>
2023-08-19 17:15:26 +0800

    add new item section placement

--- src/app/Components/ActiveListPanel.tsx
+++ src/app/Components/ActiveListPanel.tsx
@@ -7,6 +7,12 @@ export default function ActiveListPanel() {
   return (
     <aside className="p-8 bg-gray-200 flex flex-col justify-between">
       <div>
+        <section className="rounded-xl p-8 bg-gray-900 text-white mb-8">
+          <p>An item is not on the list?</p>
+          <button className="bg-white text-black mt-2 py-1 px-4 rounded-lg">
+            Add it
+          </button>
+        </section>
         <div className="flex flex-row justify-between">
           <h2 className="font-bold text-2xl">Shopping List</h2>
           <Image src={edit_list_icon} alt="Edit" height={30} />
