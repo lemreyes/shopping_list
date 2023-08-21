5388421618aa43de50e2a2ece092e4c4b52a5869
lemreyes <67998813+lemreyes@users.noreply.github.com>
2023-08-21 12:20:54 +0800

    Render master list

--- src/app/Components/Masterlist.tsx
+++ src/app/Components/Masterlist.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import Item from "./Item";
 
@@ -5,95 +6,22 @@ import Image from "next/image";
 
 import search_icon from "../../../public/assets/search_icon.svg";
 
-/////////////////////////////////////////////////
-// TEST DATA
-const MASTER_LIST = {
-  MasterList: [
-    {
-      category: "Fruits and Vegetables",
-      items: [
-        { name: "Apples", id: "1" },
-        { name: "Bananas", id: "2" },
-      ],
-      id: "1",
-    },
-    {
-      category: "Beverages",
-      items: [
-        { name: "Orange Juice", id: "1" },
-        { name: "Milk", id: "2" },
-      ],
-      id: "2",
-    },
-    { category: "Kitchen", items: [{ name: "Noodles", id: "1" }], id: "3" },
-    {
-      category: "Grains",
-      items: [
-        { name: "Ganador", id: "1" },
-        { name: "Cornflakes", id: "2" },
-      ],
-      id: "4",
-    },
-    {
-      category: "Bread",
-      items: [{ name: "Ichipan Slice Bread", id: "1" }],
-      id: "5",
-    },
-    {
-      category: "Canned Food",
-      items: [
-        { name: "Spam", id: "1" },
-        { name: "Century Tuna", id: "2" },
-        { name: "Vienna Sausage", id: "3" },
-        { name: "Hereford Corned Beef", id: "4" },
-      ],
-      id: "6",
-    },
-    {
-      category: "Cleaning",
-      items: [
-        { name: "Domex Blue Toilet Bowl Cleaner", id: "1" },
-        { name: "Domex Yellow Stain and Limescale Cleaner", id: "2" },
-        { name: "Cif All Around Cleaner", id: "3" },
-        { name: "Ariel Liquid Detergent", id: "4" },
-        { name: "Ajax Dishwashing Liquid", id: "5" },
-        { name: "Scotchbrite", id: "6" },
-      ],
-      id: "7",
-    },
-    {
-      category: "Condiments",
-      items: [
-        { name: "Golden Fiesta Palm Cooking Oil", id: "1" },
-        { name: "Iodized Salt", id: "2" },
-        { name: "Del Monte Ketchup", id: "3" },
-      ],
-      id: "8",
-    },
-    {
-      category: "Hygiene",
-      items: [
-        { name: "Old Spice High Endurance Deo", id: "1" },
-        { name: "Dove Body Soap Mens", id: "2" },
-        { name: "Modess with wings", id: "3" },
-        { name: "Carefree Pantiliners", id: "4" },
-        { name: "PH Care Feminine Wash", id: "5" },
-        { name: "Cottonbuds", id: "6" },
-        { name: "Hygienix Alcohol", id: "7" },
-      ],
-      id: "9",
-    },
-    {
-      category: "Candies",
-      items: [],
-      id: "10",
-    },
-  ],
-};
+interface Category {
+  id: string;
+  category_name: string;
+  items: Array<string>;
+}
 
-////////////////////////////////////////////////
+interface Item {
+  id: string;
+  name: string;
+}
 
-export default function Masterlist() {
+export default function Masterlist({
+  masterlist,
+}: {
+  masterlist: Array<Object>;
+}) {
   return (
     <main className="ml-16 mt-8">
       <h1 className="text-4xl ">
@@ -119,12 +47,24 @@ export default function Masterlist() {
         />
       </div>
 
-      <h3 className="text-lg font-medium mt-8">Fruit and vegetables</h3>
-      <Item label="Avocado" />
-      <Item label="Bananas" />
-      <h3 className="text-lg font-medium mt-8">Beverages</h3>
-      <Item label="Milk" />
-      <Item label="Orange Juice" />
+      {masterlist.map((category: Category | any) => {
+        console.log("category: ", category);
+        return (
+          <div key={category.id}>
+            <h3 className="text-lg font-medium mt-8">
+              {category.category_name}
+            </h3>
+            {category.items.length > 0 ? (
+              category.items.map((item: any) => {
+                console.log("Item", item);
+                return <Item key={item.id} label={item.name} />;
+              })
+            ) : (
+              <p>No items listed.</p>
+            )}
+          </div>
+        );
+      })}
     </main>
   );
 }
