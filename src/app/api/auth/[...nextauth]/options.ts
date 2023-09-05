e8724ebf5e531efdf1a77f2f888515b8234e5513
lemreyes <67998813+lemreyes@users.noreply.github.com>
2023-09-05 22:24:02 +0800

    Nextauth email provider configuration

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,8 +1,28 @@
 import type { NextAuthOptions } from "next-auth";
 
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+
+import EmailProvider from "next-auth/providers/email";
+
 let userAccount: any = null;
 
+const prisma = new PrismaClient();
+
 export const options: NextAuthOptions = {
-  providers: [],
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    EmailProvider({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+      },
+      from: process.env.EMAIL_FROM,
+    }),
+  ],
   secret: process.env.NEXTAUTH_SECRET,
 };
