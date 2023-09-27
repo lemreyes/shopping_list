c669168739b7d6b157d9a206b184af31654d5413
lemreyes <67998813+lemreyes@users.noreply.github.com>
2023-09-28 06:27:08 +0800

    BE: 1toN relation for list and listedItems

--- src/app/api/activelist/route.ts
+++ src/app/api/activelist/route.ts
@@ -27,53 +27,43 @@ export async function POST(request: Request) {
   const { listName, shoppingList } = await request.json();
   console.log("Body", listName, shoppingList);
 
-  let activeListandItems = await prisma.list.findFirst({
+  let activeList = await prisma.list.findFirst({
     where: {
       list_name: listName,
     },
   });
-  console.log("activelist first check", activeListandItems);
-
-  const listedItems = [];
-  for (let i = 0; i < shoppingList.length; i++) {
-    if (shoppingList[i].items.length > 0) {
-      for (let j = 0; j < shoppingList[i].items.length; j++) {
-        const listedItem = {
-          listed_item_name: shoppingList[i].items[j].item_name,
-          quantity: shoppingList[i].items[j].quantity,
-          is_purchased: shoppingList[i].items[j].is_purchased,
-          categoryId: shoppingList[i].id,
-        };
-        listedItems.push(listedItem);
-      }
-    }
-  }
-  console.log("Listed items: ", listedItems);
-
-  if (!activeListandItems) {
-    activeListandItems = await prisma.list.create({
+  console.log("activelist first check", activeList);
+  if (!activeList) {
+    activeList = await prisma.list.create({
       data: {
         list_name: listName,
         is_done: false,
         ownerId: userData?.id,
-        listedItems: {
-          create: listedItems,
-        },
       },
     });
-    console.log("Active list and items saved", activeListandItems);
-  } else {
-    return NextResponse.json(
-      {
-        errorMessage:
-          "A list with the same name already exists.  Please rename the list that you would like to save.",
-      },
-      { status: 400 }
-    );
+    console.log("Active list saved", activeList);
+
+    if (activeList) {
+      for (let i = 0; i < shoppingList.length; i++) {
+        if (shoppingList[i].items.length > 0) {
+          for (let j = 0; j < shoppingList[i].items.length; j++) {
+            const item = await prisma.listedItem.create({
+              data: {
+                listed_item_name: shoppingList[i].items[j].item_name,
+                quantity: shoppingList[i].items[j].quantity,
+                is_purchased: shoppingList[i].items[j].is_purchased,
+                categoryId: shoppingList[i].id,
+                listId: activeList.id,
+              },
+            });
+          }
+        }
+      }
+    }
   }
 
   return NextResponse.json({
-    activeListId: activeListandItems.id,
+    activeListId: activeList.id,
   });
 }
 
