ea1c16136b6ce91e1c9ab4c2c0f173dd4e36b934
lemreyes <67998813+lemreyes@users.noreply.github.com>
2023-10-01 20:55:07 +0800

    FE: signOut button

--- src/app/Components/ProfileMenuCard.tsx
+++ src/app/Components/ProfileMenuCard.tsx
@@ -1,11 +1,19 @@
+import { signOut } from "next-auth/react";
+
 export default function ProfileMenuCard() {
-    return (
-      <div className="absolute -top-16 desktop:top-20 right-0 bg-gray-200">
-        <ul className="w-full">
-          <li className="py-1 px-8 font-bold hover:bg-white border-b border-gray-300 w-full">Settings</li>
-          <li className="py-1 px-8 font-bold hover:bg-white">Signout</li>
-        </ul>
-      </div>
-    );
-  }
-  
+  return (
+    <div className="absolute -top-16 desktop:top-20 right-0 bg-gray-200">
+      <ul className="w-full">
+        <li className="py-1 px-8 font-bold hover:bg-white border-b border-gray-300 w-full">
+          Settings
+        </li>
+        <li
+          className="py-1 px-8 font-bold hover:bg-white"
+          onClick={() => signOut()}
+        >
+          Signout
+        </li>
+      </ul>
+    </div>
+  );
+}
