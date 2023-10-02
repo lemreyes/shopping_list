afa7cfd07d6977234eb1459071fcd2db71e90eab
lemreyes <67998813+lemreyes@users.noreply.github.com>
2023-10-02 23:17:04 +0800

    FE: List card layout WIP

--- src/app/Lists/Components/ListCard.tsx
+++ src/app/Lists/Components/ListCard.tsx
@@ -1,3 +1,6 @@
+import calendar_icon from "../../../../public/assets/calendar_icon.svg";
+import Image from "next/image";
+
 export default function ListCard({
   id,
   list_name,
@@ -12,13 +15,16 @@ export default function ListCard({
   list_url: string;
 }) {
   return (
-    <div className="border border-gray-200 p-4">
+    <article className="border border-gray-200 p-4">
       <h2 className="text-xl font-bold">{list_name}</h2>
-      <div>{updated_at.toDateString()}</div>
-      <div>{is_done}</div>
+      <div className="flex flex-row items-center">
+        <Image src={calendar_icon} alt="calendar_icon" className="w-8 mr-2" />
+        {updated_at.toDateString()}
+      </div>
+      <div className="p-2 border border-gray-600 bg-gray-200 rounded-3xl align-middle text-center">{is_done ? "Finished" : "Not finished"}</div>
       <div>
         <p>Preview is here...</p>
       </div>
-    </div>
+    </article>
   );
 }
