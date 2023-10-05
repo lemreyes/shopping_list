629dd40082b6b8388c2eb775ee0d37af902d498c
lemreyes <67998813+lemreyes@users.noreply.github.com>
2023-10-05 19:42:30 +0800

    BE: retrieve list info and list items

--- src/app/[User]/Lists/[List]/page.tsx
+++ src/app/[User]/Lists/[List]/page.tsx
@@ -1,12 +1,27 @@
 import Navbar from "@/app/Components/Navbar";
+import prisma from "../../../Utilities/prismaUtils";
+import { ListedItem } from "@prisma/client";
 
-export default function List(props: Props) {
-  console.log("searchParams", props);
-  console.log(typeof props);
+export default async function List(props: Props) {
+  const list = await prisma.list.findUnique({
+    where: {
+      id: parseInt(props.searchParams.id as string),
+    },
+  });
+
+  const listItems: Array<ListedItem> = await prisma.listedItem.findMany({
+    where: {
+      listId: parseInt(props.searchParams.id as string),
+    },
+    orderBy: {
+      categoryId: "asc",
+    },
+  });
+
   return (
     <>
       <Navbar />
-      <h1>{props.searchParams.id}</h1>
+      <h1 className="text-3xl font-bold mt-2 mb-4">{list?.list_name}</h1>
     </>
   );
 }
