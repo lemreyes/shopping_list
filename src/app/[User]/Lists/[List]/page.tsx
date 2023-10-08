718fe09816c6e3dc73181e8e8d6e502b86527e70
lemreyes <67998813+lemreyes@users.noreply.github.com>
2023-10-08 14:21:38 +0800

    BE: put auth on Lists page

--- src/app/[User]/Lists/[List]/page.tsx
+++ src/app/[User]/Lists/[List]/page.tsx
@@ -2,13 +2,32 @@ import Navbar from "@/app/Components/Navbar";
 import prisma from "../../../Utilities/prismaUtils";
 import { ListedItem } from "@prisma/client";
 import ListItem from "./Components/ListItem";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { options } from "@/app/api/auth/[...nextauth]/options";
 
 export default async function List(props: Props) {
+  const session = await getServerSession(options);
+  if (session === null) {
+    redirect("/Auth/Login");
+  }
+
+  // find user data
+  const userData = await prisma.userData.findUnique({
+    where: {
+      email: session?.user?.email as string,
+    },
+  });
+
   const list = await prisma.list.findUnique({
     where: {
       id: parseInt(props.searchParams.id as string),
+      ownerId: userData?.id,
     },
   });
+  if (!list) {
+    // TODO: error handling
+  }
 
   const listItems: Array<ListedItem> = await prisma.listedItem.findMany({
     where: {
@@ -31,17 +50,21 @@ export default async function List(props: Props) {
         <div className="w-4/5 desktop:w-1/3">
           <h1 className="text-3xl font-bold mt-2 mb-4">{list?.list_name}</h1>
           <ul>
-            {listItems.map((listItem) => {
-              return (
-                <ListItem
-                  key={listItem.id}
-                  id={listItem.id}
-                  item_name={listItem.listed_item_name}
-                  quantity={listItem.quantity}
-                  is_purchased={listItem.is_purchased}
-                />
-              );
-            })}
+            {listItems.length > 0 ? (
+              listItems.map((listItem) => {
+                return (
+                  <ListItem
+                    key={listItem.id}
+                    id={listItem.id}
+                    item_name={listItem.listed_item_name}
+                    quantity={listItem.quantity}
+                    is_purchased={listItem.is_purchased}
+                  />
+                );
+              })
+            ) : (
+              <p>No items found.</p>
+            )}
           </ul>
         </div>
       </div>
