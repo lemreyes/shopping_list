caf4e2e23ae96160fdc6c3b4a975e4286a873507
lemreyes <67998813+lemreyes@users.noreply.github.com>
2023-11-11 23:49:24 +0800

    BE: listItem route param validation

--- src/app/api/listItem/route.ts
+++ src/app/api/listItem/route.ts
@@ -4,6 +4,11 @@ import { options } from "../auth/[...nextauth]/options";
 import prisma from "../../Utilities/prismaUtils";
 import { Prisma } from "@prisma/client";
 
+interface PutRequestType {
+  listItemId: number;
+  is_purchased: boolean;
+}
+
 export async function POST(request: NextRequest) {
   const session = await getServerSession(options);
   if (!session) {
@@ -15,7 +20,15 @@ export async function POST(request: NextRequest) {
     );
   }
 
-  const { listId } = await request.json();
+  const { listId }: { listId: number } = await request.json();
+  if (listId <= 0) {
+    return NextResponse.json(
+      {
+        errorMessage: "Invalid parameters.",
+      },
+      { status: 400 }
+    );
+  }
 
   try {
     const listItems = await prisma.listedItem.findMany({
@@ -50,7 +63,7 @@ export async function PUT(request: NextRequest) {
     );
   }
 
-  const { listItemId, is_purchased } = await request.json();
+  const { listItemId, is_purchased }: PutRequestType = await request.json();
 
   try {
     const listItem = await prisma.listedItem.update({
